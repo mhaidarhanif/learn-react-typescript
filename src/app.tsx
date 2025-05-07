f8f0120a9caa6cbb8f3a9cae0539ac5a8df8738a
M Haidar Hanif <git@mhaidarhanif.com>
2025-05-07 22:39:23 +0700

    chore: create react typescript with vite

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./button";
+
+export function App() {
+  return (
+    <div>
+      <h1>Learn React TypeScript</h1>
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  );
+}
